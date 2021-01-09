e31f8eb4cb791b7dc884aefd99f0325834b3042d
jason0429hebei <1548416999@qq.com>
2021-01-09 17:44:07 +0800

    初始化

---
+++ src/test/java/rml/test/Test1.java
@@ -0,0 +1,13 @@
+package rml.test;
+
+import org.junit.Test;
+
+public class Test1 {
+
+/*	@Test
+	public void test(){
+		TestService testService = new TestServiceImpl();
+		testService.test();
+	}
+	*/
+}
